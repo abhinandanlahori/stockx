7b7e23759a57cb7d398a619173803a67116995e6
Abhinandan Lahori <abhinandanlahori@gmail.com>
2022-06-15 22:07:54 +0530

    Update page.dart

--- lib/src/pages/home/page.dart
+++ lib/src/pages/home/page.dart
@@ -47,7 +47,7 @@ class _State extends State<PageHome> {
                   bottom: BorderSide(width: 0, color: Color(0xFF000000)),
                 ),
               ),
-              child: Column(                                                      --!! "class missing"  !!--
+              child: Column(                                                     
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Container(
